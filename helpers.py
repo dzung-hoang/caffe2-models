d04174fcac9ac749422780ffd23859786795605e
Dzung Hoang <dzung.hoang@nxp.com>
2018-12-03 11:02:25 -0600

    restore license header for helpers.py

--- helpers.py
+++ helpers.py
@@ -1,10 +1,27 @@
+# Copyright (c) 2016-present, Facebook, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##############################################################################
+
+## @package helpers
+# Module caffe2.python.tutorials.helpers
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
 import numpy as np
 import skimage.io
 import skimage.transform
-import operator
-import os
-from caffe2.python import workspace, core, model_helper
-from caffe2.proto import caffe2_pb2
 
 
 def crop_center(img, cropx, cropy):
